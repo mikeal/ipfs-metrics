8e06572df398de54b6f6a2a4ccc27a92a4b96863
Mikeal Rogers <mikeal.rogers@gmail.com>
2018-12-07 14:03:31 -0800

    feat: cache repos

--- get-repos-from-bigquery.js
+++ get-repos-from-bigquery.js
@@ -1,5 +1,9 @@
+const argv = require('yargs').argv
 const { BigQuery } = require('@google-cloud/bigquery')
+const fs = require('fs')
 const path = require('path')
+
+const term = argv._[0] || 'ipfs'
 
 const projectId = 'ipfs-metrics'
 
@@ -9,7 +13,8 @@ const bigquery = new BigQuery({
   keyFilename: path.join(__dirname, 'credentials.json')
 })
 
-const sqlQuery = `
+const queries = {}
+queries.ipfs = `
 SELECT DISTINCT repo_name FROM \`bigquery-public-data.github_repos.files\` WHERE id IN
 (
   SELECT id FROM \`bigquery-public-data.github_repos.contents\`
@@ -23,15 +28,26 @@ SELECT DISTINCT repo_name FROM \`bigquery-public-data.github_repos.files\` WHERE
 `
 
 const options = {
-  query: sqlQuery,
+  query: queries[term],
   // Location must match that of the dataset(s) referenced in the query.
-  location: 'US',
-};
+  location: 'US'
+}
+
+let repos
+let f = path.join(__dirname, term) + '.json'
+
+try {
+  repos = new Set(JSON.parse(fs.readFileSync(f).toString()))
+} catch (e) {
+  console.log(`No existing data, will create ${term}.json`)
+  repos = new Set()
+}
 
 // Runs the query
 const run = async () => {
   const [rows] = await bigquery.query(options)
-  rows.forEach(row => console.log(row.repo_name))
+  rows.forEach(row => repos.add(row.repo_name))
+  fs.writeFileSync(f, JSON.stringify(Array.from(repos)))
 }
 
 run()
